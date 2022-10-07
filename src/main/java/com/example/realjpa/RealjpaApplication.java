9b393f0454086da3320d5b69db8cf4d0498fd58f
kim min woo <kbsserver@naver.com>
2022-10-07 11:41:15 +0900

    Initial commit

---
+++ src/main/java/com/example/realjpa/RealjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.realjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealjpaApplication.class, args);
+    }
+
+}
